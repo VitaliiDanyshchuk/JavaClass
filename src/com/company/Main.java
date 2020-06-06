81c29c2e876c04e1f1e46bdf21da98102dcf9443
shimko23 <shimko2323@gmail.com>
2020-06-06 15:46:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+/*
+*
+* Classame : Main
+*
+*  06 June 2020
+*
+* Copyright Danyshchuk Vitalii CHNU 543
+*  Module 2 task 1
+*
+* Create a class from the attached document according to the order in your group .  The class must contain
+1. Constructor.
+2.. Getters/Setters.
+3. 5 methods.
+4. Override toString() method
+5. Override hash() and equals() methods.
+*
+* */
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle firstTriangle = new Triangle(3,1,3);
+
+       System.out.println(firstTriangle);
+       System.out.println(firstTriangle.hashCode());
+
+       Triangle secondTriangle = new Triangle(4,4,5);
+       System.out.println(firstTriangle.equals(secondTriangle));
+    }
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,142 @@
+/*
+*
+* Classame : Triangle
+*
+*  06 June 2020
+*
+* Copyright Danyshchuk Vitalii CHNU 543
+*  Module 2 task 1
+*
+* Create a class from the attached document according to the order in your group .  The class must contain
+1. Constructor.
+2.. Getters/Setters.
+3. 5 methods.
+4. Override toString() method
+5. Override hash() and equals() methods.
+*
+* */
+package com.company;
+
+import java.util.Objects;
+
+public class Triangle {
+
+    private int firstSide;
+    private int secondSide;
+    private int thirdSide;
+
+
+    public Triangle() {
+    }
+
+    public Triangle(int firstSide, int secondSide, int thirdSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.thirdSide = thirdSide;
+    }
+
+    public int getFirstSide() {
+        return firstSide;
+    }
+    public void setFirstSide(int side) {
+        this.firstSide = side;
+    }
+
+    public int getSecondSide() {
+        return firstSide;
+    }
+    public void setSecondSide(int side) {
+        this.firstSide = side;
+    }
+
+    public int getThirdSide() {
+        return firstSide;
+    }
+    public void setThirdSide(int side) {
+        this.firstSide = side;
+    }
+
+
+
+    public double getPerimetr(){
+
+        return firstSide + secondSide +thirdSide;
+    }
+
+    public double getArea(){
+        return Math.sqrt(getPerimetr() * (getPerimetr() - getFirstSide())
+                *(getPerimetr() - getSecondSide())*
+                (getPerimetr() - getThirdSide()));
+    }
+
+    public boolean isEquilateral(){
+
+
+        boolean result;
+        if (firstSide == secondSide &&
+                firstSide == thirdSide) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public boolean isIsosceles(){
+
+
+        boolean result1;
+        if (this.getFirstSide() == this.getSecondSide() || this.getFirstSide()
+                == this.getThirdSide() || this.getSecondSide()
+                == this.getThirdSide()) {
+            result1 = true;
+        } else {
+            result1 = false;
+        }
+
+        return result1;
+    }
+
+    public int longestSide(){
+        int longestSide = 0;
+        if (getFirstSide() > longestSide)
+            longestSide = getFirstSide();
+        if (getSecondSide() > longestSide)
+            longestSide = getFirstSide();
+        if (getThirdSide() > longestSide)
+            longestSide = getFirstSide();
+        return longestSide;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Triangle:"+"\n" +
+                "first side = " + firstSide + "\n" +
+                "second side = " + secondSide + "\n" +
+                "third side = " + thirdSide + "\n" +
+                "The longest Side = " + thirdSide + "\n" +
+                "is equilateral = " + isEquilateral() + "\n" +
+                "is isosceles = " + isIsosceles() + "\n" +
+                "Area = " + this.getArea() + "\n" +
+                "Perimeter = " + this.getPerimetr();
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return getPerimetr() == triangle.getPerimetr() &&
+                Double.compare(triangle.getPerimetr(), getPerimetr()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return  Objects.hash(getFirstSide(), getSecondSide(), getThirdSide());
+    }
+
+}
